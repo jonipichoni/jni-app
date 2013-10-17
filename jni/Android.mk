697d147d890de6c967a28e1b27fec88eeaa5f356
jaime <jaime@jaime-P5K-SE-EPU.(none)>
2013-10-17 19:20:34 +0100

    mod: application refactored

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,19 +1,42 @@
-LOCAL_PATH:= $(call my-dir)
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-# Local App
-LOCAL_MODULE    := libgl2jni
+LOCAL_MODULE    := native-activity
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include \
+					$(LOCAL_PATH)/libs
+                   # $(LOCAL_PATH)/include/glm
+                
 
-# TEST_APP defines which TEST to RUN
-LOCAL_CFLAGS    := -Werror -DTEST_APP=1
-LOCAL_SRC_FILES := gl_code.cpp
-LOCAL_LDLIBS    := -llog -lGLESv2
+LOCAL_SRC_FILES := 	src/main.cpp \
+		   			src/GLESApplication.cpp \
+                   	src/GLESDemo.cpp \
+                   	src/Cube.cpp \
+                   	src/ShaderProgram.cpp \
+                   	src/res_texture.c
 
-# 3rd party libs
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/libs
+#LOCAL_LDFLAGS	:= -Wl,-Map=native-activity.map  
 
-# for native asset manager
-LOCAL_LDLIBS += -landroid
+
+LOCAL_LDLIBS    := -llog  -landroid -lEGL -lGLESv2
+
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
 
 include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)
+
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,2 +1,5 @@
-# GNUSTL needed for GLM lib support
+# APP_ABI := armeabi x86
+APP_PLATFORM := android-9
+APP_OPTIM := debug
+APP_CFLAGS := -DDEBUG 
 APP_STL := gnustl_static
